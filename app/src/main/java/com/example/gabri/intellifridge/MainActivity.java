e1b76b38c7084096a68570b6e10cd25fa91629a7
Gabriel Boroghina <gabib97@yahoo.com>
2018-03-24 14:45:12 +0200

    UI Login

--- app/src/main/java/com/example/gabri/intellifridge/MainActivity.java
+++ app/src/main/java/com/example/gabri/intellifridge/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.gabri.intellifridge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean registered = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!registered) {
+            Intent login = new Intent(getApplicationContext(), LoginActivity.class);
+            //startActivity(login);
+        }
     }
 }
---
+++ app/src/main/java/com/example/gabri/intellifridge/SignupActivity.java
@@ -0,0 +1,8 @@
+package com.example.gabri.intellifridge;
+
+/**
+ * Created by gabri on 3/24/2018.
+ */
+
+public class SignupActivity {
+}
